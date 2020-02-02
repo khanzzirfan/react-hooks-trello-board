f4ec49c156c1136c317b2312135639c4ee9832f6
khanzzirfan <khanzz_irfan@hotmail.com>
2020-02-02 21:19:39 +1100

    chore(build): committed build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.053084208ee819f4e238fe597eec19fa.js"
+  "/react-hooks-trello-board/precache-manifest.89f112881865836217ba03d2f1edc75a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-hooks-trello-board/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
