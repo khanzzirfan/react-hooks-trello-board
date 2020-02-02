e16e1de87835e98bdcf3b2b0e6bc3907cffa3fd6
khanzzirfan <khanzz_irfan@hotmail.com>
2020-02-02 21:06:36 +1100

    chore(build): build folder for gh pages

---
+++ build/precache-manifest.053084208ee819f4e238fe597eec19fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3933ecf3db83b32573b",
+    "url": "/static/js/main.546dd8ed.chunk.js"
+  },
+  {
+    "revision": "c0b57772ebd9849b634d",
+    "url": "/static/js/2.e622d843.chunk.js"
+  },
+  {
+    "revision": "f3933ecf3db83b32573b",
+    "url": "/static/css/main.0308c5c7.chunk.css"
+  },
+  {
+    "revision": "e7bc1e51b2aa143c2db2e02b27bfb398",
+    "url": "/index.html"
+  }
+];
