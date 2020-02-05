9c8a3727044765b9e4477ae1921091c15a926811
khanzzirfan <khanzz_irfan@hotmail.com>
2020-02-05 18:55:19 +1100

    chore(cleanup): removed unused files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
-import counterReducer from './counter'
 import lists from './lists'
 
 const rootReducer = history =>
   combineReducers({
-    counter: counterReducer,
     lists,
     router: connectRouter(history)
   })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
-// import Home from '../containers/home'
 import { Base } from '../containers/Base'
-
-// import Hello from '../containers/Hello'
-// import Counter from '../containers/Counter'
-// import NoMatch from '../containers/NoMatch'
-// import NavBar from '../containers/NavBar'
 
 const routes = (
   <div>
