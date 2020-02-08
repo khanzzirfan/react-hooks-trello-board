839d52b68c57bee38804ca1020ea65b248d7a235
khanzzirfan <khanzz_irfan@hotmail.com>
2020-02-08 18:15:41 +1100

    chore(refactor): removed unused vars

--- src/containers/Board/Cards/CardsContainer.jsx
+++ src/containers/Board/Cards/CardsContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
-import { DropTarget, DragSource, useDrag, useDrop } from 'react-dnd'
+import { useDrag, useDrop } from 'react-dnd'
 import Cards from './Cards'
 import { CardTypes } from './cardTypes'
 
